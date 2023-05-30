74ff02160bf47d745f4121043e4099567a9821bc
Ayush Singhal <ayushsinghals02@gmail.com>
2023-05-30 17:19:52 +0530

    Changed UI in allNotesActivity and Update Delete Activity

--- app/src/main/java/com/ayushsinghal/notesapp/ExpandedNoteActivity.kt
+++ app/src/main/java/com/ayushsinghal/notesapp/ExpandedNoteActivity.kt
@@ -1,17 +1,113 @@
 package com.ayushsinghal.notesapp
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.window.OnBackInvokedDispatcher
 import com.ayushsinghal.notesapp.databinding.ActivityExpandedNoteBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 
 class ExpandedNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExpandedNoteBinding
+    private lateinit var onBackPressedCallback: OnBackPressedCallback
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExpandedNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.expandedNoteTitleID.text = intent.getStringExtra("TITLE")
-        binding.expandedNoteDescriptionID.text = intent.getStringExtra("DESCRIPTION")
+        if (binding.expandedNoteDescriptionID.text.isEmpty()) {
+            binding.expandedNoteDescriptionID.visibility = View.GONE
+        } else {
+            binding.expandedNoteDescriptionID.visibility = View.VISIBLE
+            binding.expandedNoteDescriptionID.maxLines = Integer.MAX_VALUE
+        }
+
+        binding.expandedNoteTitleID.setText(intent.getStringExtra("TITLE"))
+        binding.expandedNoteDescriptionID.setText(intent.getStringExtra("DESCRIPTION"))
+
+        binding.deleteButton.setOnClickListener() {
+            val noteId = intent.getStringExtra("NOTE ID")
+            val noteRef =
+                Firebase.database.getReference("Notes").child(Firebase.auth.currentUser!!.uid)
+                    .child(noteId!!)
+//            noteRef.removeValue()
+//            finish()
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Confirm Delete")
+            builder.setMessage("Do you want to delete this note?")
+            builder.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id ->
+                val dbRef =
+                    Firebase.database.getReference("Notes").child(Firebase.auth.uid!!)
+                        .child(noteId)
+                Toast.makeText(this, "Note Deleted Successfully", Toast.LENGTH_SHORT).show()
+                dbRef.removeValue()
+                dialog.cancel()
+                finish()
+            })
+
+            builder.setNegativeButton("No", DialogInterface.OnClickListener { dialog, id ->
+                dialog.cancel()
+            })
+            val alert = builder.create()
+            alert.show()
+
+        }
+
+        binding.backButton.setOnClickListener() {
+            saveUpdatedNote()
+            finish()
+        }
+
+        onBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+
+                    saveUpdatedNote()
+                    finish()
+                }
+            }
+
+        // Add the onBackPressedCallback to the activity's onBackPressedDispatcher
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    }
+
+    private fun saveUpdatedNote() {
+        val dbRef = Firebase.database.getReference("Notes")
+            .child(Firebase.auth.currentUser!!.uid)
+
+        if ((binding.expandedNoteTitleID.text.toString() == "") && (binding.expandedNoteDescriptionID.text.toString() == "")) {
+//                        binding.errorTextView.text = "Both Title and Description can not be empty!!!"
+        } else {
+            val userID = Firebase.auth.currentUser?.uid
+            val noteID = intent.getStringExtra("NOTE ID")
+            val dataModel = DataModel(
+                binding.expandedNoteTitleID.text.toString(),
+                binding.expandedNoteDescriptionID.text.toString(),
+                userID!!,
+                noteID!!
+            )
+            dbRef.child(noteID).setValue(dataModel)
+                .addOnCompleteListener { result ->
+                    if (result.isSuccessful) {
+//                        Toast.makeText(this@ExpandedNoteActivity, "Note Updated Successfully", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            this@ExpandedNoteActivity,
+                            "Note can not be Updated",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/com/ayushsinghal/notesapp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/ayushsinghal/notesapp/RecyclerViewAdapter.kt
@@ -1,30 +1,23 @@
 package com.ayushsinghal.notesapp
 
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.ayushsinghal.notesapp.databinding.ActivityExpandedNoteBinding
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+
 
 class RecyclerViewAdapter(val context: Context, val itemList: ArrayList<DataModel>) :
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var noteTitle: TextView = itemView.findViewById(R.id.noteTitle)
+        var noteDescription: TextView = itemView.findViewById(R.id.noteDescription)
 
-        var deleteButton: Button = itemView.findViewById(R.id.deletebtn)
-        var updateButton: Button = itemView.findViewById(R.id.updatebtn)
         var cardView: CardView = itemView.findViewById(R.id.cardView)
     }
 
@@ -41,46 +34,15 @@ class RecyclerViewAdapter(val context: Context, val itemList: ArrayList<DataMode
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.noteTitle.text = itemList[position].title
-
-        holder.deleteButton.setOnClickListener()
-        {
-            val noteId = itemList[position].noteID
-//            Toast.makeText(context, noteId, Toast.LENGTH_SHORT).show()
-            val builder = AlertDialog.Builder(context)
-            builder.setTitle("Confirm Delete")
-            builder.setMessage("Do you want to delete this note?")
-            builder.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id ->
-                val dbRef =
-                    Firebase.database.getReference("Notes").child(Firebase.auth.uid!!)
-                        .child(noteId!!)
-                Toast.makeText(context, "Note Deleted Successfully", Toast.LENGTH_SHORT).show()
-                dbRef.removeValue()
-                dialog.cancel()
-            })
-
-            builder.setNegativeButton("No", DialogInterface.OnClickListener { dialog, id ->
-                dialog.cancel()
-            })
-            val alert = builder.create()
-            alert.show()
-
-        }
-
-        holder.updateButton.setOnClickListener()
-        {
-            val intent = Intent(context, AddUpdateNoteActivity::class.java)
-            intent.putExtra("MODE", "UPDATE")
-            intent.putExtra("TITLE", itemList[position].title)
-            intent.putExtra("DESCRIPTION", itemList[position].description)
-            intent.putExtra("NOTE ID", itemList[position].noteID)
-            context.startActivity(intent)
-        }
+        holder.noteDescription.text = itemList[position].description
 
         holder.cardView.setOnClickListener()
         {
             val intent = Intent(context, ExpandedNoteActivity::class.java)
             intent.putExtra("TITLE", itemList[position].title)
             intent.putExtra("DESCRIPTION", itemList[position].description)
+            intent.putExtra("NOTE ID", itemList[position].noteID)
+            
             context.startActivity(intent)
         }
 
