b68ff795be5973322198908a5c7aae92a4ffb51d
Ayush Singhal <ayushsinghals02@gmail.com>
2023-06-03 19:42:53 +0530

    Added Capitalization of first letter of both title and description text fields in AddNoteActivity, and Added testSaveNoteInBulk() function

--- app/src/main/java/com/ayushsinghal/notesapp/AddNoteActivity.kt
+++ app/src/main/java/com/ayushsinghal/notesapp/AddNoteActivity.kt
@@ -8,6 +8,11 @@ import com.ayushsinghal.notesapp.databinding.ActivityAddNoteBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.random.Random
 
 class AddNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddNoteBinding
@@ -19,6 +24,8 @@ class AddNoteActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.noteDescriptionID.requestFocus()
+
+//        testSaveNoteInBulk()
 
         binding.backButton.setOnClickListener()
         {
@@ -56,13 +63,44 @@ class AddNoteActivity : AppCompatActivity() {
             dbRef.child(noteID).setValue(dataModel)
                 .addOnCompleteListener { result ->
                     if (result.isSuccessful) {
-                        Toast.makeText(this, "Note Added Successfully", Toast.LENGTH_SHORT)
-                            .show()
+//                        Toast.makeText(this, "Note Added Successfully", Toast.LENGTH_SHORT)
+//                            .show()
                     } else {
                         Toast.makeText(this, "Note can not be added", Toast.LENGTH_SHORT)
                             .show()
                     }
                 }
         }
+    }
+
+    private fun testSaveNoteInBulk() {
+        val dbRef = Firebase.database.getReference("Notes").child(Firebase.auth.currentUser!!.uid)
+        val userID = Firebase.auth.currentUser?.uid
+
+        GlobalScope.launch {
+            for (i in 1..50) {
+                withContext(Dispatchers.IO) {
+                    // Perform database or network operations in a background thread
+                    val randomNumber = Random.nextInt(20, 100)
+                    val randomNumber2 = Random.nextInt(200, 1000)
+                    val noteID = dbRef.child(userID!!).push().key
+                    val dataModel = DataModel(
+                        getRandomString(randomNumber),
+                        getRandomString(randomNumber2),
+                        userID,
+                        noteID!!
+                    )
+                    dbRef.child(noteID).setValue(dataModel)
+                }
+            }
+        }
+
+    }
+
+    fun getRandomString(length: Int): String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return (1..length)
+            .map { allowedChars.random() }
+            .joinToString("")
     }
 }
